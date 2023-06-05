99c4d57bf397bfb3fcbe5e1638eb7380aaf49a67
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-06-05 23:52:02 +0200

    some work on the identity part

--- generate.sh
+++ generate.sh
@@ -4,9 +4,9 @@
 pdflatex --output-directory=src src/main.tex
 bibtex src/main
 pdflatex --output-directory=src src/main.tex
-pdflatex --output-directory=dist src/main.tex
-cp dist/main.pdf dist/lakat.pdf
-rm dist/main.*
+pdflatex --output-directory=src src/main.tex
+cp src/main.pdf dist/lakat.pdf
+# rm dist/main.*
 # cp dist/lakat.pdf dist/lakat-$(date +%Y-%m-%d).pdf
 # cp dist
 
