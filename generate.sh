67452b63d76b5d7a8e6b62bdf8f8ff30082ec4da
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-06-12 16:22:35 +0200

    new generate script

--- generate.sh
+++ generate.sh
@@ -6,7 +6,10 @@ bibtex src/main
 pdflatex --output-directory=src src/main.tex
 pdflatex --output-directory=src src/main.tex
 cp src/main.pdf dist/lakat.pdf
-# rm dist/main.*
+cp src/main.tex src/temp_main.tex
+rm src/main.*
+cp src/temp_main.tex src/main.tex
+rm src/temp_main.tex
 # cp dist/lakat.pdf dist/lakat-$(date +%Y-%m-%d).pdf
 # cp dist
 
